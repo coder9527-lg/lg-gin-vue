cb6b58a70837a2a42ad4eb519b7c33b8a28cdd3f
NUC <nuc@NUCdeMac-mini.local>
2022-05-02 00:57:43 +0800

    add:register

--- controller/userhandler.go
+++ controller/userhandler.go
@@ -7,6 +7,14 @@ import (
 )
 
 func Register(c *gin.Context) {
+	//get the parameter
+	name := c.PostForm("name")
+	password := c.PostForm("password")
+	phone := c.PostForm("phone")
+
+	//Data verification
+	//Create user
+	//Return result
 	c.JSON(http.StatusOK, gin.H{
 		"msg": "Register success",
 	})
